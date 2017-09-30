3613068bf1c627fcbe3fe500320440a6b71d0090
Jean-Serge Monbailly <jean-serge@derniercri.io>
2017-10-01 00:08:00 +0200

    Use procs

--- src/charcount_nim.nim
+++ src/charcount_nim.nim
@@ -4,13 +4,19 @@ type CharCount = array[256, int]
 
 const s = "aazauizuiufayuzfabzyeuofbzduyvbihiufauhi"
 
-var count: CharCount
+proc char_count(text: string): CharCount =
+  var count: CharCount
+  for c in text:
+    let index = int(c)
+    inc(count[index])
+  
+  count
 
-for c in s:
-  let index = int(c)
-  inc(count[index])
+proc print_count(count: CharCount) =
+  for index, x in count.pairs:
+    if x != 0:
+      let current_char = char(index)
+      echo "Character '$1' appears $2 times in the text." % [$current_char, $x]
 
-for index, x in count.pairs:
-  if x != 0:
-    let current_char = char(index)
-    echo "Character '$1' appears $2 times in the text." % [$current_char, $x]
+let count = char_count(s)
+print_count(count)
