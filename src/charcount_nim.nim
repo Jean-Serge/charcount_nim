1f3ea8fd5c0c88937862fe267f41deb58cacb30f
Jean-Serge Monbailly <jean-serge@derniercri.io>
2017-09-30 23:50:04 +0200

    Use proc to convert types instead of cast

--- src/charcount_nim.nim
+++ src/charcount_nim.nim
@@ -7,9 +7,9 @@ const s = "aazauizuiufayuzfabzyeuofbzduyvbihiufauhi"
 var count: CharCount
 
 for c in s:
-  inc(count[cast[int](c)])
+  inc(count[int(c)])
 
 for index, x in count.pairs:
   if x != 0:
-    let current_char = cast[char](index)
+    let current_char = char(index)
     echo "Character '$1' appears $2 times in the text." % [$current_char, $x]
